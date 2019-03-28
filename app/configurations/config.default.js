916e01119a737eafd4775b153edce066fc80ba37
Vesa Meskanen <vesa.meskanen@cgi.com>
2019-03-28 11:40:43 +0200

    Enable citybike routing

--- app/configurations/config.default.js
+++ app/configurations/config.default.js
@@ -357,7 +357,7 @@ export default {
     },
 
     citybike: {
-      availableForSelection: false, // TODO: Turn off in autumn
+      availableForSelection: true, // TODO: Turn off in autumn
       defaultValue: false, // always false
     },
   },

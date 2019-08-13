81d8d8c9ab54497853bff170cf7fcea8c00ee10d
Maximilian Richt <maxi@richt.name>
2019-08-13 03:44:40 +0200

    allow img and other tags in about text

--- app/component/AboutPage.js
+++ app/component/AboutPage.js
@@ -6,8 +6,9 @@ import { FormattedMessage } from 'react-intl';
 import connectToStores from 'fluxible-addons-react/connectToStores';
 import SanitizedHTML from 'react-sanitized-html';
 
-const AboutPage = ({ currentLanguage }, { config }) => {
-  const about = config.aboutThisService[currentLanguage];
+const AboutPage = ({ currentLanguage }, context) => {
+  const about = context.config.aboutThisService[currentLanguage];
+  const allowedTags = ['a', 'b', 'i', 'strong', 'em', 'img', 'br'];
   return (
     <div className="about-page fullscreen">
       <div className="page-frame fullscreen momentum-scroll">
@@ -19,7 +20,7 @@ const AboutPage = ({ currentLanguage }, { config }) => {
                 <h1 className="about-header">{section.header}</h1>
                 {section.paragraphs &&
                   section.paragraphs.map((p, j) => (
-                    <p key={`about-section-${i}-p-${j}`}><SanitizedHTML html={ p } /></p>
+                    <p key={`about-section-${i}-p-${j}`}><SanitizedHTML allowedTags={ allowedTags } html={ p } /></p>
                   ))}
                 {section.link && (
                   <a href={section.link}>

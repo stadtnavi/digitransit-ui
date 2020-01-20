c7c2024a03b7093c55f881b6a9a7f9c0b9ea4b61
Beck-berry <trebeka98@gmail.com>
2020-01-20 13:47:18 +0100

    add Matomo Code

--- app/util/analyticsUtils.js
+++ app/util/analyticsUtils.js
@@ -31,7 +31,22 @@ export function addAnalyticsEvent(event) {
  */
 export function getAnalyticsInitCode(GTMid, MTM_URL) {
   if (MTM_URL) {
-    return `<!-- Matomo Tag Manager -->
+    return `<!-- Matomo -->
+      <script type="text/javascript">
+        var _paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+          var u="https://"+"${MTM_URL}";
+          _paq.push(['setTrackerUrl', u+'matomo.php']);
+          _paq.push(['setSiteId', '1']);
+          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+          g.type='text/javascript'; g.async=true; g.defer=true; g.src='//cdn.matomo.cloud/'+'${MTM_URL}'+'matomo.js'; s.parentNode.insertBefore(g,s);
+        })();
+      </script>
+      <!-- End Matomo Code -->
+      <!-- Matomo Tag Manager -->
       <script type="text/javascript">
       var _mtm = _mtm || [];
       _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});

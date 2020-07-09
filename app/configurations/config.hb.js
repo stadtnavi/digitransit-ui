2d2b72cc7375b17415e8ed55f42c2751528e72db
Beck-berry <trebeka98@gmail.com>
2020-07-09 08:33:26 +0200

    Revert "add 'uri-demo' citybike network to config"
    This reverts commit b5294d341436601d467f7f4dee8dddd5ec7657e2.

--- app/configurations/config.hb.js
+++ app/configurations/config.hb.js
@@ -126,18 +126,6 @@ export default configMerger(walttiConfig, {
           en: 'https://www.regioradstuttgart.de/',
         },
       },
-      "uri-demo": {
-        icon: 'regiorad',
-        name: {
-          de: 'RegioRad',
-          en: 'RegioRad',
-        },
-        type: 'citybike',
-        url: {
-          de: 'https://www.regioradstuttgart.de/de',
-          en: 'https://www.regioradstuttgart.de/',
-        },
-      },
       taxi: {
         icon: 'taxi',
         name: {
@@ -154,7 +142,6 @@ export default configMerger(walttiConfig, {
         },
         type: 'car-sharing',
       },
-
     }
   },
 

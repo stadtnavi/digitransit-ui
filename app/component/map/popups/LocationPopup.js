f412413d760492c7bf53883a12e9987e1889e55d
Leonard Ehrenfried <mail@leonard.io>
2021-03-10 08:46:44 +0100

    Reactivate defect reporter for BW

--- app/component/map/popups/LocationPopup.js
+++ app/component/map/popups/LocationPopup.js
@@ -136,8 +136,8 @@ class LocationPopup extends React.Component {
     } = this.state.location;
     const { lat, lon } = this.props;
     // when the BW-wide Mängelmelder is up we can use its URL, too
-    // let reporterUrl = `https://maengelmelder.service-bw.de/?lat=${lat}&lng=${lon}`;
-    let reporterUrl = null;
+    let reporterUrl = `https://maengelmelder.service-bw.de/?lat=${lat}&lng=${lon}`;
+    // let reporterUrl = null;
     if (postalcode === '71083') {
       reporterUrl = `https://www.herrenberg.de/tools/mvs/?lat=${lat}&lng=${lon}#mvPagePictures`;
     }

8094c0be130ccedd69a5fd34c64e2492359e4568
Leonard Ehrenfried <mail@leonard.io>
2021-03-13 12:24:40 +0100

    Use correct value for BICYCLE_PARK query

--- app/configurations/config.default.js
+++ app/configurations/config.default.js
@@ -352,7 +352,7 @@ export default {
     airplane: 'AIRPLANE',
     ferry: 'FERRY',
     walk: 'WALK',
-    bicycle: 'BICYCLE_PARK',
+    bicycle: 'BICYCLE_PARK,WALK',
     car: 'CAR',
     car_park: 'CAR_PARK',
     public_transport: 'WALK',

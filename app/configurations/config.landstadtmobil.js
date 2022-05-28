2dbcad7020838411782a57a40ef2483fa0eb6875
Holger Bruch <hb@mfdz.de>
2022-05-28 08:29:38 +0200

    Update feedback address

--- app/configurations/config.landstadtmobil.js
+++ app/configurations/config.landstadtmobil.js
@@ -85,7 +85,7 @@ export default configMerger(walttiConfig, {
 
     appBarLink: {
         name: 'Feedback',
-        href: 'mailto:J.Baisch-Nipatsiripol@kreis-reutlingen.de?subject=landstadtmobil Feedback',
+        href: 'mailto:nachhaltige-entwicklung@kreis-reutlingen.de?subject=landstadtmobil Feedback',
         target: '_blank'
     },
 

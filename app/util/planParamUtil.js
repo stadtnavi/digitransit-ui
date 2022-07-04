41df972bc3839685b2c63b4717b56c83729dd974
Leonard Ehrenfried <mail@leonard.io>
2022-07-04 10:33:04 +0200

    Update numItineraries

--- app/util/planParamUtil.js
+++ app/util/planParamUtil.js
@@ -311,7 +311,7 @@ export const preparePlanParams = (config, useDefaultModes) => (
         from: fromLocation,
         to: toLocation,
         intermediatePlaces: intermediatePlaceLocations,
-        numItineraries: 10,
+        numItineraries: 5,
         date: parsedTime.format('YYYY-MM-DD'),
         time: parsedTime.format('HH:mm:ss'),
         walkReluctance: settings.walkReluctance,

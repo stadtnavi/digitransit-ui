efe64ff5da2d400a33a82607dfb83ce9cc7ba086
Holger Bruch <hb@mfdz.de>
2024-11-17 15:55:56 +0100

    Change innenstadtnavi to stadtnavi

--- app/configurations/config.aachen.js
+++ app/configurations/config.aachen.js
@@ -2,7 +2,7 @@
 import configMerger from '../util/configMerger';
 
 const CONFIG = 'aachen';
-const APP_TITLE = 'Innenstadtnavi Aachen';
+const APP_TITLE = 'stadtnavi Aachen';
 const APP_DESCRIPTION = 'Gemeinsam Mobilität neu denken - die intermodale Verbindungssuche mit offenen, lokalen Daten';
 const API_URL = process.env.API_URL || 'https://api.aachen.stadtnavi.eu';
 const OTP_URL = process.env.OTP_URL || `${API_URL}/otp/routers/default/`;

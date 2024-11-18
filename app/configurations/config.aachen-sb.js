77910fe275a1c09848045b7255d931cf2037c6a5
Holger Bruch <hb@mfdz.de>
2024-11-18 17:40:48 +0100

    set APP_TITLE to Zukunftsrouting Aachen

--- app/configurations/config.aachen-sb.js
+++ app/configurations/config.aachen-sb.js
@@ -2,7 +2,7 @@
 import configMerger from '../util/configMerger';
 
 const CONFIG = 'aachen-sb';
-const APP_TITLE = 'Innenstadtnavi Aachen';
+const APP_TITLE = 'Zukunftsrouting Aachen';
 const APP_DESCRIPTION = 'Gemeinsam Mobilität neu denken - die intermodale Verbindungssuche mit offenen, lokalen Daten';
 const API_URL = process.env.API_URL || 'https://api.aachen-sb.stadtnavi.eu';
 const OTP_URL = process.env.OTP_URL || `${API_URL}/otp/routers/default/`;

0a02657933c97936f7e1745c5f356fc580c492ed
Holger Bruch <hb@mfdz.de>
2025-01-04 13:49:01 +0100

    Disable layer controls

--- app/configurations/config.mitfahren-bw.js
+++ app/configurations/config.mitfahren-bw.js
@@ -65,6 +65,9 @@ export default configMerger(parentConfig, {
             'satellite': '© <a tabindex=-1 href=http://osm.org/copyright>OpenStreetMap Mitwirkende</a>, © <a tabindex=-1 href="https://www.lgl-bw.de/">LGL BW</a>, <a tabindex=-1 href=https://www.nvbw.de/aufgaben/digitale-mobilitaet/open-data/>Datensätze der NVBW GmbH</a>',
             'bicycle': '© <a tabindex=-1 href=http://osm.org/copyright>OpenStreetMap Mitwirkende</a>, © <a tabindex=-1 href=https://www.cyclosm.org/#map=12/52.3728/4.8936/cyclosmx>CyclOSM</a>, © <a tabindex=-1 href="https://www.openstreetmap.fr/">OSM-FR</a>, <a tabindex=-1 href=https://www.nvbw.de/aufgaben/digitale-mobilitaet/open-data/>Datensätze der NVBW GmbH</a>',
         },
+        // for now, we disable layers
+        showZoomControl: false,
+        showLayerSelector: false,
     },
 
     //textLogo: true,

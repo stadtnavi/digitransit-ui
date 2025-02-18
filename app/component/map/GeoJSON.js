7bf94a43494d0548b363c38ecb62d9c982f37379
Holger Bruch <hb@mfdz.de>
2025-02-18 18:01:40 +0100

    feat: add school route map

--- app/component/map/GeoJSON.js
+++ app/component/map/GeoJSON.js
@@ -122,7 +122,7 @@ class GeoJSON extends React.Component {
     const { config } = this.context;
     const defaultLineStyle = {
       className: 'cursor-grab',
-      color: config.colors.primary,
+      color: config.colors.geojsonLineColor || config.colors.primary,
       weight: 3,
       opacity: 0.8,
     };
